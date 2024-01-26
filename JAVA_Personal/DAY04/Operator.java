dcce6e0360e739c38c1d21f64b03e0fc17dfdd25
jiyoung79 <155033243+jiyoung79@users.noreply.github.com>
2024-01-27 02:03:04 +0900

    Add files via upload

---
+++ JAVA_Personal/DAY04/Operator.java
@@ -0,0 +1,22 @@
+package day04;
+
+public class Operator {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 3;
+		double doubleB = 3; //자동형변환
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		System.out.println(a/doubleB);
+		// 정수/실수 = 실수 (컴퓨터가 실수를 표현할 때 정확하게 표현할 수 없다.)
+		//				왜? 무한한 실수의 갯수를 유한한 표현방법으로 표현하기에는 한계가 있기 때문
+		System.out.println(a%doubleB); // 정수%실수 = 실수
+		System.out.println(doubleB%b); // 실수%정수 = 실수
+		
+	}
+
+}
---
+++ JAVA_Personal/DAY04/Sign_operator.java
@@ -0,0 +1,12 @@
+package day04;
+
+public class Sign_operator {
+	public static void main(String[] args) {
+		int a = -1;
+		int b = 2;
+		System.out.println(-a);
+		System.out.println(-b);
+
+	}
+
+}
