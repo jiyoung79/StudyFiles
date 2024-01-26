04d816dcca0cf68ec8f0524dd798e62a0fe8a90a
jiyoung79 <155033243+jiyoung79@users.noreply.github.com>
2024-01-27 01:55:23 +0900

    Add files via upload

---
+++ JAVA_Personal/DataType.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class DataType {
+	public static void main(String[] args) {
+		int a = 65;
+		float f = a;	//묵시적형변환(자동형변환)
+		System.out.println(f);
+		char c = (char)a;	//명시적형변환(강제형변환)
+		System.out.println(c);
+		// 아스키코드값 : 문자마다 고유한 숫자값을 부여 A=65, a=97
+		// 강제형변환 방법? (자료형)변수명;
+
+	}
+
+}
