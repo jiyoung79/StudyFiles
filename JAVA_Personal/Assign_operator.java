871905244f4d9e75c0bc755cf766d343e76c3b6c
jiyoung79 <155033243+jiyoung79@users.noreply.github.com>
2024-01-27 02:07:25 +0900

    Add files via upload

---
+++ JAVA_Personal/Assign_operator.java
@@ -0,0 +1,20 @@
+package day06;
+
+public class Assign_operator {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		
+		b = a; // b = 3
+		System.out.println(b);
+		
+		a += 1; // a=a+1
+		System.out.println(a);
+		
+		a /= 2; // a=a/2
+		System.out.println(a);
+		
+		a *= a;
+		System.out.println(a);
+	}
+}
