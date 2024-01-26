83d501e8249d21ddaa921426d22f88e406a780be
jiyoung79 <155033243+jiyoung79@users.noreply.github.com>
2024-01-27 02:24:06 +0900

    Update For03.java

--- JAVA_Personal/DAY09/For03.java
+++ JAVA_Personal/DAY09/For03.java
@@ -9,3 +9,18 @@ public class For03 {
 			}
 	}
 }
+/*
+Console
+
+i=1, sum=1
+i=2, sum=3
+i=3, sum=6
+i=4, sum=10
+i=5, sum=15
+i=6, sum=21
+i=7, sum=28
+i=8, sum=36
+i=9, sum=45
+i=10, sum=55
+
+*/
