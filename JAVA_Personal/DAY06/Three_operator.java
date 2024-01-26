d9163782a47fd53fdebf3229a0155ad849b584bf
jiyoung79 <155033243+jiyoung79@users.noreply.github.com>
2024-01-27 02:08:50 +0900

    Add files via upload

---
+++ JAVA_Personal/DAY06/Three_operator.java
@@ -0,0 +1,9 @@
+package day06;
+
+public class Three_operator {
+	public static void main(String[] args) {
+			int age = 20; //정수형 변수에 값 저장
+			System.out.println(age>19?"성인입니다":"청소년입니다"); 
+	}
+
+}
