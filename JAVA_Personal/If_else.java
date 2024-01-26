8b8082379241b94268e03aa1b73b99f244cba695
jiyoung79 <155033243+jiyoung79@users.noreply.github.com>
2024-01-27 02:11:18 +0900

    Add files via upload

---
+++ JAVA_Personal/If_else.java
@@ -0,0 +1,16 @@
+package day07;
+
+public class If_else {
+	public static void main(String[] args) {
+		int age = 15;
+		if(age>19) {
+			System.out.println("성인입니다.");
+			System.out.println("성인 요금이 적용됩니다.");
+		} 
+		else { //age<=10
+			System.out.println("미성년자입니다.");
+			System.out.println("청소년 요금이 적용됩니다.");
+		}
+		System.out.println("결제를 진행해 주세요.");
+	}
+}
