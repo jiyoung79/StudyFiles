59fa4476c39d4b446c264a273170eb2873dbd928
jiyoung79 <155033243+jiyoung79@users.noreply.github.com>
2024-01-27 01:57:05 +0900

    Add files via upload

---
+++ JAVA_Personal/DAY03/Prca.java
@@ -0,0 +1,12 @@
+package day03;
+
+public class Prca {
+	public static void main(String[] args) {
+		int a = 3;
+		a = 4;
+		final double PI = 3.14;
+		// 변수의 상수화 방법? final 자료형 변수명; -> 변수명은 모든 글자를 대문자로 만들어준다.
+
+	}
+
+}
